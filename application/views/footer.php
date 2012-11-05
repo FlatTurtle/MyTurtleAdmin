d63262096e6e6194de06a7273dedb874e934dec8
Pieter Colpaert <pieterc@member.fsf.org>
2012-11-05 02:03:10 +0100

    Added footer message and a hidden source comment for possible developers

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,11 +1,20 @@
 			</div>
 		</div>
 	</div>
-	<footer role='footer'>
+	<footer role="footer">
 		<div class="container">
 			<div class="row">
 				<div class="span12">
-					&copy; FlatTurtle bvba
+					&copy; 2012 <a href="https://flatturtle.com" target="_blank">FlatTurtle</a> - Some rights reserved
+<!--
+                                                   Are you a developer who would like to know more about FlatTurtle?
+                                                   Did you know you can control your FlatTurtle through an API?
+                                                   Did you know you can write your own turtles and panes?
+                                                   Get in touch with us at 
+                                                      http://dev.FlatTurtle.com 
+                                                   or mail us at 
+                                                      info@flatturtle.com
+-->
 				</div>
 			</div>
 		</div>
