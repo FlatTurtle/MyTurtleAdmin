e05d5a4f4719f38a8391abbd7c7c61db4f28bfef
Michiel Vancoillie <info@ninetynine.be>
2012-11-21 14:15:57 +0100

    Add MIVB turtle

---
+++ assets/inc/turtles/options_mivb.php
@@ -0,0 +1,16 @@
+<? include 'header.php'; ?>
+<div class="control-group">
+    <label class="control-label" for="{{id}}-location">Location</label>
+    <div class="controls">
+		<input type="text" id="{{id}}-location" name="{{id}}-location" placeholder="Name of a stop (ex. Gent Veergrep, 1658)" class='input-block-level autocomplete' value="{{location}}"/>
+	</div>
+</div>
+<div class="control-group">
+    <label class="control-label" for="{{id}}-limit">Items to show</label>
+    <div class="controls">
+		<select name="{{id}}-limit" class='input-small'>
+			{{limit-options}}
+		</select>
+	</div>
+</div>
+<? include 'footer.php'; ?>
