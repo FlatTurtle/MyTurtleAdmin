95e2056dda32810327b530082c58069b5c8eed42
Pieter Colpaert <pieter@irail.be>
2013-01-27 08:20:05 -0800

    Translated MyTurtleAdmin to Dutch

---
+++ application/language/dutch/main_lang.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * Header
+ */
+
+// Page <title>
+$lang['title'] = "Uw FlatTurtle";
+
+// Greeting in front of name
+$lang['greeting'] = "Dag";
+
+
+
+/**
+ * Login page
+ */
+$lang['login.title'] = "Gelieve in te loggen";
+
+
+/**
+ * General terms
+ */
+$lang['term.save'] = "Bewaren";
+$lang['term.cancel'] = "Annuleren";
+$lang['term.send'] = "Zenden";
+$lang['term.browse'] = "Bladeren";
+$lang['term.log_in'] = "Log in";
+$lang['term.log_out'] = "Log uit";
+$lang['term.retry'] = "Opnieuw";
+$lang['term.delete'] = "Verwijder";
+$lang['term.disable'] = "Uitschakelen";
+
+
+$lang['term.infoscreens'] = "Infoschermen";
+$lang['term.left'] = "Links";
+$lang['term.right'] = "Rechts";
+$lang['term.username'] = "Gebruikersnaam";
+$lang['term.password'] = "Wachtwoord";
+$lang['term.title'] = "Titel";
+$lang['term.address'] = "Adres";
+$lang['term.color'] = "Kleur";
+$lang['term.footer'] = "Onderkant";
+$lang['term.logo'] = "Logo";
+$lang['term.hostname'] = "Naam installatie";
+$lang['term.none'] = "Geen";
+$lang['term.message'] = "Bericht";
+$lang['term.updates'] = "Updates";
+$lang['term.location'] = "Locatie";
+$lang['term.duration'] = "Duur";
+$lang['term.seconds'] = "Seconden";
+$lang['term.primary'] = "Eerste";
+$lang['term.secondary'] = "Tweede";
+$lang['term.search'] = "Zoeken";
+$lang['term.feed'] = "Feed";
+
+
+/**
+ * Screen detail page
+ */
+$lang['screen.sent_message'] = "Stuur een bericht";
+$lang['screen.sent_message_note'] = "Dit bericht werd even op het scherm geplaatst.";
+$lang['screen.sent_message_alt'] = "Toon een bericht op het scherm";
+$lang['screen.toggle_clock_alt'] = "Zet de klok aan of uit";
+$lang['screen.toggle_screen_alt'] = "Zet het scherm aan of uit";
+
+$lang['screen.left_side'] = "Linkerkant";
+$lang['screen.right_side'] = "Rechterkant";
+$lang['screen.address_alt'] = "Locatie (bvb: Wetstraat 1, Brussel)";
+$lang['screen.geographic_coordinates'] = "Geographische coördinaten";
+$lang['screen.pin_for_tablet'] = "Pincode voor de tablet";
+
+
+/**
+ * Turtles page
+ */
+$lang['turtles.available'] = "Beschikbare Turtles";
+$lang['turtles.drag_to_screen'] = "Sleep een Turtle naar het scherm";
+$lang['turtles.screen_title'] = "Linkerkant van het scherm";
+$lang['turtles.drag_to_sort'] = "Sleep de Turtles om ze te sorteren";
+$lang['turtles.option_number_of_items'] = "Elementen om weer te geven;
+$lang['turtles.option_zoom'] = "Zoom";
+$lang['turtles.error_blank'] = "Er zijn geen te configureren opties voor deze Turtle.";
+
+/**
+ * Specific for a turtle
+ */
+$lang['turtle.airport_alt'] =  "Luchthavencode (bvb. BRU or AMS)";
+$lang['turtle.delijn_alt'] =  "Haltenaam (bvb. Grimbergen Dorp)";
+$lang['turtle.mivb_alt'] =  "Haltenaam (bvb. Mozart)";
+$lang['turtle.nmbs_alt'] =  "Haltenaam (bvb. Gent Sint-Pieters)";
+$lang['turtle.velo_alt'] =  "Naam van een Velo plaats (bvb. Theater)";
+$lang['turtle.villo_alt'] =  "Naam van een Villo plaats (bvb. Botanique/Kruidtuin)";
+$lang['turtle.mapbox_alt'] =  "Adres van een locatie (bvb. Gent Korenmarkt)";
+$lang['turtle.finance_primary_alt'] = "(bvb. KBC.BR)";
+$lang['turtle.finance_secondary_alt'] = "(bvb. GOOG, AAPL, DUV.BR)";
+$lang['turtle.finance_primary_note'] = "Aandeelsysmbool, deze staat het grootst op het scherm";
+$lang['turtle.finance_secondary_note'] = "Een lijst van aandeelsymbolen gescheiden door een komma";
+$lang['turtle.twitter_search_alt'] = "Twitter (bvb. #FlatTurtle)";
+$lang['turtle.rss_alt'] = "Een URL naar een RSS-feed";
+$lang['turtle.rss_custom'] = "Eigen feed";
+$lang['turtle.screen_location'] = "Een andere locatie";
+$lang['turtle.custom_location'] = "Ander";
+
+
+/**
+ * Panes page
+ */
+$lang['panes.available'] = "Beschikbare panes";
+$lang['panes.enabled'] = "Ingeschakelde panes";
+$lang['panes.screen_title'] = "Rechterkant van het scherm";
+$lang['panes.drag_to_sort'] = "Sleep panes om te sorten";
+$lang['panes.click_to_enable'] = "Klik om een pane in te schakelen";
+$lang['panes.general_options'] = "Algemene pane opties";
+
+
+/**
+ * Error messages
+ */
+$lang['error.logo_upload'] = "Er ging iets fout bij het opladen van een nieuwe foto. Gelieve opnieuw te proberen.";
+$lang['error.color_check_hex'] = "De kleur {{value}} is geen hexadecimaal getal.";
+$lang['error.resolve_address'] = "Kon geen locatie vinden voor de opgegeven tekst. Kunt u me iets specifieker geven?";
+
+/**
+ * Footer
+ */
+
+// Rights reserved
+$lang['footer.rights'] = "Sommige rechten voorbehouden";
