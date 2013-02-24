a82ea610e42dac955e128aa0d0cfac5fb547f44c
Michiel Vancoillie <info@ninetynine.be>
2013-02-25 00:48:11 +0100

    Small fix for version 1.1 API

--- application/libraries/securelogin.php
+++ application/libraries/securelogin.php
@@ -2,7 +2,7 @@
 
 /**
  * FlatTurtle bvba
- * @author: Michiel Vancoillie 
+ * @author: Michiel Vancoillie
  */
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
@@ -20,7 +20,7 @@ define('PHPASS_HASH_PORTABLE', true);
 class SecureLogin {
 
     var $CI;
-    var $user_table = 'customer';
+    var $user_table = 'user';
 
     /**
      * Create a user account
@@ -52,7 +52,7 @@ class SecureLogin {
 
         $this->CI->db->set($data);
 
-        if (!$this->CI->db->insert($this->user_table)) //There was a problem! 
+        if (!$this->CI->db->insert($this->user_table)) //There was a problem!
             return false;
 
         if ($auto_login)
@@ -60,31 +60,31 @@ class SecureLogin {
 
         return true;
     }
-    
-    
+
+
     function change_password($user_id = '', $user_pass = ''){
         $this->CI = & get_instance();
-        
+
          //Make sure account info was sent
         if ($user_id == '' OR $user_pass == '') {
             return false;
         }
-        
+
          //Check against user table
         $this->CI->db->where('id', $user_id);
         $query = $this->CI->db->get_where($this->user_table);
 
         if ($query->num_rows() != 1)
             return false;
-        
+
         //Hash user_pass using phpass
         $hasher = new PasswordHash(PHPASS_HASH_STRENGTH, PHPASS_HASH_PORTABLE);
         $user_pass_hashed = $hasher->HashPassword($user_pass);
-        
-        
+
+
         $this->CI->db->where('id', $user_id);
         $this->CI->db->update($this->user_table, array('password' => $user_pass_hashed));
-        
+
         return true;
 
     }
@@ -92,7 +92,7 @@ class SecureLogin {
     /**
      * Login and sets session variables
      */
-	
+
     function login($user_name = '', $user_pass = '', $session_name='logged_in') {
         $this->CI = & get_instance();
 
@@ -122,7 +122,7 @@ class SecureLogin {
 
             //Create a fresh, brand new session
             $this->CI->session->sess_create();
-			
+
             //Set session data
 			$user_data['password'] = $user_pass;
             $user_data[$session_name] = true;
