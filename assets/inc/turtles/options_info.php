1f161bee92f89baa3fc78b9aa228bf12b61a799e
Michiel Vancoillie <info@ninetynine.be>
2013-04-16 03:43:47 +0200

    Add info turtle

---
+++ assets/inc/turtles/options_info.php
@@ -0,0 +1,7 @@
+<? include 'header.php'; ?>
+<textarea id="{{id}}-data" name="{{id}}-data" rows="10" class="wysihtml5">{{data}}</textarea>
+
+<a href='#' class="btn turtle_save">{{term.save}}</a><i class='loading fade'></i>
+        </form>
+    </div>
+</div>
--- assets/inc/turtles/options_villo.php
+++ assets/inc/turtles/options_villo.php
@@ -2,9 +2,9 @@
 <div class="control-group">
     <label class="control-label" for="{{id}}-location">{{term.location}}</label>
     <div class="controls">
-		<input type="text" id="{{id}}-name" name="{{id}}-name" placeholder="{{turtle.villo_alt}}" class='input-block-level autocomplete villo-name' value="{{name}}"/>
-		<input type="hidden" id="{{id}}-location" name="{{id}}-location" class='input-block-level villo-location' value="{{location}}"/>
+        <input type="text" id="{{id}}-name" name="{{id}}-name" placeholder="{{turtle.villo_alt}}" class='input-block-level autocomplete villo-name' value="{{name}}"/>
+        <input type="hidden" id="{{id}}-location" name="{{id}}-location" class='input-block-level villo-location' value="{{location}}"/>
         <input type="hidden" id="{{id}}-time_walk" name="{{id}}-time_walk" class='vommp-time_walk time_walk' value="{{time_walk}}"/>
-	</div>
+    </div>
 </div>
 <? include 'footer.php'; ?>
