b2bf76baf7895c02e9fe58be051268fb55cdb377
Michiel Vancoillie <info@ninetynine.be>
2013-05-22 01:21:59 +0200

    Added guzzle

--- application/models/option.php
+++ application/models/option.php
@@ -5,24 +5,15 @@
  * @author: Michiel Vancoillie
  */
 if (!defined('BASEPATH'))
-	exit('No direct script access allowed');
+    exit('No direct script access allowed');
 
 class Option extends CI_Model {
 
-	private $API;
-
-	public function __construct() {
-		parent::__construct();
-		$this->API = $this->config->item('api_url');
-	}
-
-	/**
-	 * Get all panes
-	 */
-	public function get($name = null) {
-		return json_decode($this->api->get($this->API . API_OPTION . "/?name=" . urlencode($name)));
-	}
+    /**
+     * Get all panes
+     */
+    public function get($name = null) {
+        return $this->api->get(API_OPTION . "/?name=" . urlencode($name));
+    }
 
 }
-
-?>
