93915d5e53c759514b6165063add55411288fa8f
Pieter Colpaert <pieter@irail.be>
2013-05-26 14:15:00 +0300

    Also use shots in image href
    Closes #141

--- application/views/screen/advanced/shots.php
+++ application/views/screen/advanced/shots.php
@@ -8,7 +8,7 @@
 
                 <div class='infoscreen'>
                     <div class='screen'>
-                        <a href='<?= site_url($infoscreen->alias.'/shot/'.$shot->name); ?>' target="_blank">
+                        <a href='<?= site_url($infoscreen->alias.'/shots/'.$shot->name); ?>' target="_blank">
                             <img src="data:image/jpg;base64,<?= $shot->data ?>" />
                         </a>
                         <br/>
