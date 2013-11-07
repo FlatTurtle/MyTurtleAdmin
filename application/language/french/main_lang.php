7385c36a123b3c5c07f914e9d6e599b980756861
Nik Torfs <niktorfs@gmail.com>
2013-11-07 21:19:01 +0100

    correcting french

--- application/language/french/main_lang.php
+++ application/language/french/main_lang.php
@@ -199,7 +199,7 @@ $lang['error_create_turtle'] = "Impossible de créer le Turtle. Veuillez actuali
 $lang['error_delete_turtle'] = "Impossible de supprimer le Turtle pour le moment";
 $lang['error_add_pane'] = "Impossible d'activer le panneau. Veuillez actualiser l'écran et réessayer";
 $lang['error_delete_pane'] = "Impossible de supprimer le panneau pour le moment";
-$lang['error_resolve_walktime_geocodes'] = "Impossible de calculer la durée de marche. Les coordonnées géographiques de l'écran ne sont pas définies"
+$lang['error_resolve_walktime_geocodes'] = "Impossible de calculer la durée de marche. Les coordonnées géographiques de l'écran ne sont pas définies";
 
 /**
  * Warnings
